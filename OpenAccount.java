169d95c8752c524fcd9330e2c11d4e865a954de8
PeterNunez <117960440+PeterNunez@users.noreply.github.com>
2022-12-06 12:10:15 -0800

    Add files via upload

---
+++ OpenAccount.java
@@ -0,0 +1,60 @@
+public class OpenAccount {
+    
+    private String FirstName;
+    private String LastName;
+    private String EmailAddress;
+    private String SocialSecurityNumber;
+
+
+    public OpenAccount(String FirstName, String LastName, String EmailAddress, String SocialSecurityNumber ) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.EmailAddress = EmailAddress;
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        EmailAddress = emailAddress;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public String toString() {
+        return FirstName+ " : "+ LastName+ " : " +EmailAddress+ " : " +SocialSecurityNumber;
+    }
+
+    
+
+
+    
+
+    
+
+    
+}
