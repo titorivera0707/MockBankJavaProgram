8c33923673ae5e9288691bbe1e431599745b86bf
Tito Rivera <titorivera0707@gmail.com>
2022-12-14 19:05:38 -0800

    Fixed merge conflicts I think.

--- App.java
+++ App.java
@@ -7,7 +7,6 @@ public class App {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         Scanner keyboard = new Scanner(System.in);
 
         //accept menu method
@@ -16,7 +15,7 @@ public class App {
             //accessing the menu in this switch statement
 
             Person person = new Person(null, null, null, null, null);
-            Account newAccount = new Account(0, null);
+            Account newAccount = new Account(0.0, null);
 
             Scanner input = new Scanner(System.in);
 
@@ -57,7 +56,8 @@ public class App {
                break;
                
                case 2:
-               System.out.println(Bank.getAll());
+               Bank.list();
+               System.out.println("\n");
                break;
                
                case 3:
@@ -116,103 +116,6 @@ public class App {
                break;
                
                case 5:
-=======
-      Scanner keyboard = new Scanner(System.in);
-
-      //accept menu method
-      int userSelect;
-      do{
-          //accessing the menu in this switch statement
-
-          Scanner input = new Scanner(System.in);
-
-          //A list of options for the user
-          System.out.println("1 - Open Account");
-          System.out.println("2 - List Accounts");
-          System.out.println("3 - Deposit funds");
-          System.out.println("4 - Withdraw funds");
-          System.out.println("5 - Close an account");
-          System.out.println("6 - Exit");
-
-          //Asking user to pick an option to start the program
-          System.out.print("\nHello, welcome to CS Bank and Trust. \nPlease enter your choice: ");
-          userSelect = input.nextInt();
-
-          //each case is equlivent to each number choice in the menu
-          switch(userSelect) {
-             case 1:
-             Person person = new Person(null, null, null, null, null);
-             Account newAccount = new Account(0.0, "Open");
-             Interface in = Bank.bankInterface(person, newAccount);
-             System.out.print("\nEnter first name: ");
-             person.setFirstName(keyboard.nextLine());
-
-             System.out.print("Enter last name: ");
-             person.setLastName(keyboard.nextLine());
-
-             System.out.print("Enter Social security number: ");
-             person.setSSN(keyboard.nextLine());
-
-             System.out.print("Account Name: ");
-             person.setAccountName(keyboard.nextLine());
-
-             newAccount.setAccountStatus("Open");
-
-             System.out.println("Thank you, the account number is: " +in.getAccountNumber()+".\n");
-             break;
-             
-             case 2:
-             Bank.list();
-             System.out.println("\n");
-             break;
-             
-             case 3:
-             
-             System.out.print("\nEnter account number: ");
-             int searchString = keyboard.nextInt();
-
-             System.out.print("Enter the amount to deposit: ");
-             double newDep = keyboard.nextDouble();
-             Bank.depos(searchString, newDep);
-
-             if(Bank.find(searchString) == 0) {
-                System.out.print("Account not found ");
-             }
-             else{
-             Bank.find(searchString);
-             System.out.print("Deposit successful, the new balance is: $" + Bank.find(searchString) + "\n");
-             }
-             break;
-             
-
-             case 4:
-
-             System.out.print("\nEnter account number: ");
-             int accountNum = keyboard.nextInt();
-
-             System.out.print("Enter the amount to withdraw: ");
-             double Withdraw = keyboard.nextDouble();
-
-             if (Bank.find(accountNum) < Withdraw) {
-                System.out.println("You can't withdraw more than you have. Try again.");
-                break;
-             }
-             else{
-                Bank.withd(accountNum, Withdraw);
-             }
-
-             if(Bank.find(accountNum)==0) {
-                System.out.print("Account not found ");
-             }
-             else{
-             Bank.find(accountNum);
-             System.out.print("Withdraw successful, the new balance is: $" + Bank.find(accountNum) + "\n");
-             }
-             break;
-             
-             case 5:
-             Account newNewAccount = new Account(0.0, "Open");
->>>>>>> e3cf8e481a190070b3fc4ecf9a771a76fd2fe5e1
                System.out.print("\nEnter account number to close: ");
                int accNumber = keyboard.nextInt();
 
@@ -225,23 +128,19 @@ public class App {
                   newAccount.setAccountStatus(Bank.search(accNumber));
                   System.out.println("Account closed, current balance is "+Bank.find(accNumber)+" deposits are no longer possible");
                }
-             break;
+               break;
 
-             case 6:
-             System.exit(0);
-             break;
+               case 6:
+               System.exit(0);
+               break;
 
-             default:
-             break;
-          }
+               default:
+               break;
+            }
 
-      //Error checking incase user uses invalid numbers.
-      } while(userSelect > 0 && userSelect < 7);
+        //Error checking incase user uses invalid numbers.
+        } while(userSelect > 0 && userSelect < 7);
 
      }
-<<<<<<< HEAD
 
 }
-=======
-}
->>>>>>> e3cf8e481a190070b3fc4ecf9a771a76fd2fe5e1
--- Interface.java
+++ Interface.java
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-
-
-public class Interface {
-
-    //Creating variables for the classes that have been created.
-    private int accountNumber;
-    private static int accountCounter = 1000;
-    private Person accHolder;
-    private Account bankAccount;
-    private int balance;
-
-    public Interface(Person accHolder, Account bankAccount){
-
-        super();
-        this.balance = balance;
-        this.accountNumber = accountCounter++;
-        this.accHolder = accHolder;
-        this.bankAccount = bankAccount;
-
-    }
-
-    //Setting getters and setters.
-    public int getBalance() {
-        return balance;
-    }
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-    public int getAccountCounter() {
-        return accountCounter;
-    }
-    public void setAccountCounter(int accountCounter) {
-        this.accountCounter = accountCounter;
-    }
-    public Person getAccHolder() {
-        return accHolder;
-    }
-    public void setAccHolder(Person accHolder) {
-        this.accHolder = accHolder;
-    }
-    public Account getBankAccount() {
-        return bankAccount;
-    }
-    public void setBankAccount(Account bankAccount) {
-        this.bankAccount = bankAccount;
-    }
-
-    //Creates a string to call whenever full account information is requested.
-    public String toString() {
-
-        return getAccountNumber() + " " + accHolder.toString() + " : " + getBalance() + " : Account - " + bankAccount.getAccountStatus();
-
-    }
-
-}
-=======
 public class Interface {
       
        //Creating variables for the classes that have been created.
@@ -121,4 +58,3 @@ public class Interface {
     }
   
 }
->>>>>>> e3cf8e481a190070b3fc4ecf9a771a76fd2fe5e1
